c334d83abf8e0e28fbfade922127c2af27455fac
yihwankim <kimleehwan3@gmail.com>
2021-03-26 16:06:13 +0900

    210326

--- selenium-2021-03-25-main.py
+++ selenium-2021-03-25-main.py
@@ -91,27 +91,79 @@ long = []  # 경도
 code = []  # 아파트 코드
 # office_number = []
 # add = [] #주소
+
 ### 새롭게 추가 ###
-area = [] #면적 : 공급/전용(전용률)
-room = [] #방 갯수
-toilet = [] #화장실 개수
-n_this_area = [] #해당면적 세대수
 
-
-
-
+#1
+area1 = [] #면적 : 공급/전용(전용률)
+room1 = [] #방 갯수
+toilet1 = [] #화장실 개수
+struc1 = [] #현관구조
+n_this_area1 = [] #해당면적 세대수
+#2
+area2 = [] #면적 : 공급/전용(전용률)
+room2 = [] #방 갯수
+toilet2 = [] #화장실 개수
+struc2 = [] #현관구조
+n_this_area2 = [] #해당면적 세대수
+#3
+area3 = [] #면적 : 공급/전용(전용률)
+room3 = [] #방 갯수
+toilet3 = [] #화장실 개수
+struc3 = [] #현관구조
+n_this_area3 = [] #해당면적 세대수
+#4
+area4 = [] #면적 : 공급/전용(전용률)
+room4 = [] #방 갯수
+toilet4 = [] #화장실 개수
+struc4 = [] #현관구조
+n_this_area4 = [] #해당면적 세대수
+#5
+area5 = [] #면적 : 공급/전용(전용률)
+room5 = [] #방 갯수
+toilet5 = [] #화장실 개수
+struc5 = [] #현관구조
+n_this_area5 = [] #해당면적 세대수
+#6
+area6 = [] #면적 : 공급/전용(전용률)
+room6 = [] #방 갯수
+toilet6 = [] #화장실 개수
+struc6 = [] #현관구조
+n_this_area6 = [] #해당면적 세대수
+#7
+area7 = [] #면적 : 공급/전용(전용률)
+room7 = [] #방 갯수
+toilet7 = [] #화장실 개수
+struc7 = [] #현관구조
+n_this_area7 = [] #해당면적 세대수
+#8
+area8 = [] #면적 : 공급/전용(전용률)
+room8 = [] #방 갯수
+toilet8 = [] #화장실 개수
+struc8 = [] #현관구조
+n_this_area8 = [] #해당면적 세대수
+#9
+area9 = [] #면적 : 공급/전용(전용률)
+room9 = [] #방 갯수
+toilet9 = [] #화장실 개수
+struc9 = [] #현관구조
+n_this_area9 = [] #해당면적 세대수
+#10
+area10 = [] #면적 : 공급/전용(전용률)
+room10 = [] #방 갯수
+toilet10 = [] #화장실 개수
+struc10 = [] #현관구조
+n_this_area10 = [] #해당면적 세대수
 
 #####################
-### 리허설
+### 리허설 ###########
 #####################
 chrome = webdriver.Chrome('chromedriver.exe')
 apt = '제기동 이수브라운'
 chrome.get('https://land.naver.com/') # 네이버 부동산 실행
 time.sleep(1)
-# apt = df_name[0]
-# Copy selector을 해서 원하는 '검색창'의 정보를 불러온다.
-# queryInputHeader = 해당 검색창의 selector
-input = chrome.find_element_by_css_selector('#queryInputHeader')
+
+input = chrome.find_element_by_css_selector('#queryInputHeader') #검색창 선택
 input.clear()
 input.send_keys(apt)  # enter 키를 누르기 전 상태
 # input.submit()
@@ -123,31 +175,36 @@ link.click()
 
 time.sleep(1)
 #기본정보append
-number_selector = "#detailContents1 > div.detail_box--complex > table > tbody > tr:nth-child(1) > td:nth-child(2)"
-number.append(chrome.find_element_by_css_selector(number_selector).text)
-floor_selector = "#detailContents1 > div.detail_box--complex > table > tbody > tr:nth-child(1) > td:nth-child(4)"
-floor.append(chrome.find_element_by_css_selector(floor_selector).text)
-confirm_date_selector = "#detailContents1 > div.detail_box--complex > table > tbody > tr:nth-child(2) > td:nth-child(2)"
-confirm_date.append(chrome.find_element_by_css_selector(confirm_date_selector).text)
-car_selector = "#detailContents1 > div.detail_box--complex > table > tbody > tr:nth-child(2) > td:nth-child(4)"
-car.append(chrome.find_element_by_css_selector(car_selector).text)
-FAR_selector = "#detailContents1 > div.detail_box--complex > table > tbody > tr:nth-child(3) > td:nth-child(2)"
-FAR.append(chrome.find_element_by_css_selector(FAR_selector).text)
-BC_selector = "#detailContents1 > div.detail_box--complex > table > tbody > tr:nth-child(3) > td:nth-child(4)"
-BC.append(chrome.find_element_by_css_selector(BC_selector).text)
-con_selector = "#detailContents1 > div.detail_box--complex > table > tbody > tr:nth-child(4) > td"
-con.append(chrome.find_element_by_css_selector(con_selector).text)
-heat_selector = "#detailContents1 > div.detail_box--complex > table > tbody > tr:nth-child(5) > td"
-heat.append(chrome.find_element_by_css_selector(heat_selector).text)
+collect()
 
-#area = [] #면적 : 공급/전용(전용률)
-#room = [] #방 갯수
-#toilet = [] #화장실 개수
-#n_this_area = [] #해당면적 세대수
+#area'i' = [] #면적 : 공급/전용(전용률)
+#room'i' = [] #방 갯수
+#toilet'i' = [] #화장실 개수
+#struc'i' = [] #현관구조
+#n_this_area'i' = [] #해당면적 세대수
 
-num1 = chrome.find_element_by_css_selector("#tab0 > span")
-num1.click()
-area
+for i in range(10):
+    if i==0:
+        area1_selector = "#tabpanel > table > tbody > tr:nth-child(1) > td"
+        area1.append(chrome.find_element_by_css_selector(area1_selector).text)
+
+        rt1_selector = "#tabpanel > table > tbody > tr:nth-child(2) > td"  # 방 개수와 화장실 개수
+        rt1 = chrome.find_element_by_css_selector(rt1_selector).text
+        room1.append(rt1.split('/')[0])
+        toilet1.append(rt1.split('/')[1])
+
+        struc1_selector = "#tabpanel > table > tbody > tr:nth-child(4) > td"
+        struc1.append(chrome.find_element_by_css_selector(struc1_selector).text)
+
+        n_this_area1_selector = "#tabpanel > table > tbody > tr:nth-child(3) > td"
+        n_this_area1(chrome.find_element_by_css_selector(n_this_area1_selector).text)
+
+    else:
+        try:
+            num2 = chrome.find_element_by_css_selector("#tab1 > span")
+            num2.click()
+
+
 #tab1 > span
 #tab2 > span
 
