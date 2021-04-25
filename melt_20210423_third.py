f22506442ae75409a8d4ef6c945103e048fdfaba
yihwankim <kimleehwan3@gmail.com>
2021-04-25 23:22:17 +0900

    210425 edit_4까지 완료

--- melt_20210423_third.py
+++ melt_20210423_third.py
@@ -12,13 +12,13 @@ import numpy as np
 # 강북 엑셀파일 불러오기
 
 filenames_GB = [
-    'Dobonggu_edit3', 'Dongdaemoongu_edit3',
-    'Eunpyeonggu_edit3', 'Gangbukgu_edit3',
-    'Gwangjingu_edit3', 'Jongnogu_edit3',
-    'Junggu_edit3', 'Jungnanggu_edit3',
-    'Mapogu_edit3', 'Nowongu_edit3',
-    'Seodaemungu_edit3', 'Seongbukgu_edit3',
-    'Seongdonggu_edit3', 'Yongsangu_edit3'
+    'Dobonggu_edit4', 'Dongdaemoongu_edit4',
+    'Eunpyeonggu_edit4', 'Gangbukgu_edit4',
+    'Gwangjingu_edit4', 'Jongnogu_edit4',
+    'Junggu_edit4', 'Jungnanggu_edit4',
+    'Mapogu_edit4', 'Nowongu_edit4',
+    'Seodaemungu_edit4', 'Seongbukgu_edit4',
+    'Seongdonggu_edit4', 'Yongsangu_edit4'
 ]
 
 dfs = []
@@ -26,8 +26,8 @@ dfs = []
 for fname in filenames_GB:
     print('Loading {}'.format(fname))
 
-    df = pd.read_excel('Gangbuk_edit3/{}.xlsx'.format(fname))
-    #df.columns = [fname]
+    df = pd.read_excel('Gangbuk_edit4/{}.xlsx'.format(fname))
+    # df.columns = [fname]
 
     dfs.append(df)
 
@@ -35,192 +35,112 @@ print('Data loading is completed!')
 
 df_GB = pd.concat(dfs, axis=0)  # axis=0 : 밑으로 붙이기
 
+# 강남 엑셀파일 불러오기
+
+filenames_GN = [
+    'Dongjakgu_edit4', 'Gangdonggu_edit4',
+    'Gangnamgu_edit4', 'Gangseogu_edit4',
+    'Geumcheongu_edit4', 'Gurogu_edit4',
+    'Gwanakgu_edit4', 'Seochogu_edit4',
+    'Songpagu_edit4', 'Yangcheongu_edit4',
+    'Yeongdeungpogu_edit4']
+
+dfs2 = []
+
+for fname in filenames_GN:
+    print('Loading {}'.format(fname))
+
+    df = pd.read_excel('Gangnam_edit4/{}.xlsx'.format(fname))
+    # df.columns = [fname]
+
+    dfs2.append(df)
+
+print('Data loading is completed!')
+
+df_GN = pd.concat(dfs2, axis=0)  # axis=0 : 밑으로 붙이기
+
 ###########################################################################################
 
-df_GB_data = pd.read_excel('Gangbuk_edit3/Dobonggu_edit3.xlsx')
+# 강북데이터와 강남데이터를 각각 edit4 폴더에 새롭게 저장한 후 합치기
 
-df_GB_ = pd.read_excel('Gangbuk_edit3/Dobonggu_edit3.xlsx', usecols='A:P')
+df_GB.to_excel('Gangbuk_edit4/Gangbuk_total.xlsx', sheet_name='Gangbuk', index=False)
+df_GN.to_excel('Gangnam_edit4/Gangnam_total.xlsx', sheet_name='Gangnam', index=False)
 
-#############################################################################################################
-# 6개의 컬럼으로 나누어진 면적별 정보를 하나의 칼럼으로 합쳐서 시리즈로 저장하기
-# 해당 시리즈는 앞으로 10개 이상의 시리즈로 확장되어야함
-se_type1 = df_data['type_capacity1'].str.cat(df_data[['area1',
-                                                      'room1',
-                                                      'toilet1',
-                                                      'structure1',
-                                                      'n_this_area1']], sep=',')
+df_seoul = pd.concat([df_GB, df_GN], axis=0)
 
-se_type2 = df_data['type_capacity2'].str.cat(df_data[['area2',
-                                                      'room2',
-                                                      'toilet2',
-                                                      'structure2',
-                                                      'n_this_area2']], sep=',')
+# seoul data 편집하기
 
-se_type3 = df_data['type_capacity3'].str.cat(df_data[['area3',
-                                                      'room3',
-                                                      'toilet3',
-                                                      'structure3',
-                                                      'n_this_area3']], sep=',')
+df_seoul.replace('-', np.nan)
 
-se_type4 = df_data['type_capacity4'].str.cat(df_data[['area4',
-                                                      'room4',
-                                                      'toilet4',
-                                                      'structure4',
-                                                      'n_this_area4']], sep=',')
+df = df_seoul[['Gu', '읍면동', 'Apt_name']]
 
-se_type5 = df_data['type_capacity5'].str.cat(df_data[['area5',
-                                                      'room5',
-                                                      'toilet5',
-                                                      'structure5',
-                                                      'n_this_area5']], sep=',')
+# number 편집
+df['세대수'] = df_seoul['number'].str.split('(', n=1, expand=True)
+df['세대수'] = df['세대수'].str.slice(start=0, stop=-2)
+df['세대수'] = pd.to_numeric(df['세대수'])
 
-se_type6 = df_data['type_capacity6'].str.cat(df_data[['area6',
-                                                      'room6',
-                                                      'toilet6',
-                                                      'structure6',
-                                                      'n_this_area6']], sep=',')
+# floor 편집
+df[['저층', '고층']] = df_seoul['floor'].str.split('/', expand=True)
+df['저층'] = df['저층'].str.slice(start=0, stop=-1)
+df['저층'] = pd.to_numeric(df['저층'])
+df['고층'] = df['고층'].str.slice(start=0, stop=-1)
+df['고층'] = pd.to_numeric(df['고층'])
 
-se_type7 = df_data['type_capacity7'].str.cat(df_data[['area7',
-                                                      'room7',
-                                                      'toilet7',
-                                                      'structure7',
-                                                      'n_this_area7']], sep=',')
+# car 편집
+df[['주차대수_총', '주차대수_세대']] = df_seoul['car'].str.split('(', n=1, expand=True)
+df['주차대수_총'] = df['주차대수_총'].str.slice(start=0, stop=-1)
+df['주차대수_총'] = pd.to_numeric(df['주차대수_총'])
 
-se_type8 = df_data['type_capacity8'].str.cat(df_data[['area8',
-                                                      'room8',
-                                                      'toilet8',
-                                                      'structure8',
-                                                      'n_this_area8']], sep=',')
+df['주차대수_세대'] = df['주차대수_세대'].str.slice(start=4, stop=-2)
+df['주차대수_세대'] = pd.to_numeric(df['주차대수_세대'])
 
-se_type9 = df_data['type_capacity9'].str.cat(df_data[['area9',
-                                                      'room9',
-                                                      'toilet9',
-                                                      'structure9',
-                                                      'n_this_area9']], sep=',')
+# FAR (용적률) 편집
+df['용적률'] = pd.to_numeric(df_seoul['FAR'].str.slice(start=0, stop=-1))
 
-se_type10 = df_data['type_capacity10'].str.cat(df_data[['area10',
-                                                        'room10',
-                                                        'toilet10',
-                                                        'structure10',
-                                                        'n_this_area10']], sep=',')
+# BC (건폐율) 편집
+df['건폐율'] = pd.to_numeric(df_seoul['BC'].str.slice(start=0, stop=-1))
 
-se_type11 = df_data['type_capacity11'].str.cat(df_data[['area11',
-                                                        'room11',
-                                                        'toilet11',
-                                                        'structure11',
-                                                        'n_this_area11']], sep=',')
+# 건설사, 난방
+df[['건설사', '난방']] = df_seoul[['con', 'heat']]
 
-se_type12 = df_data['type_capacity12'].str.cat(df_data[['area12',
-                                                        'room12',
-                                                        'toilet12',
-                                                        'structure12',
-                                                        'n_this_area12']], sep=',')
+# 아파트코드, 위도, 경도
+df['아파트코드'] = pd.to_numeric(df_seoul['code'])
+df['위도'] = pd.to_numeric(df_seoul['lat'])
+df['경도'] = pd.to_numeric(df_seoul['long'])
 
-se_type13 = df_data['type_capacity13'].str.cat(df_data[['area13',
-                                                        'room13',
-                                                        'toilet13',
-                                                        'structure13',
-                                                        'n_this_area13']], sep=',')
+# 면적정보
+df['면적유형'] = df_seoul['type_capacity']
 
-se_type14 = df_data['type_capacity14'].str.cat(df_data[['area14',
-                                                        'room14',
-                                                        'toilet14',
-                                                        'structure14',
-                                                        'n_this_area14']], sep=',')
+# 크기
+area_inform = df_seoul['area'].str.split('/')
+df['공급면적(㎡)'] = area_inform.str.get(0)
+df['공급면적(㎡)'] = df['공급면적(㎡)'].str.slice(start=0, stop=-1)
+df['공급면적(㎡)'] = pd.to_numeric(df['공급면적(㎡)'])  # 오류발생
 
-se_type15 = df_data['type_capacity15'].str.cat(df_data[['area15',
-                                                        'room15',
-                                                        'toilet15',
-                                                        'structure15',
-                                                        'n_this_area15']], sep=',')
+area_inform2 = area_inform.str.get(1)
+area_inform2 = area_inform2.str.split('(')
 
-se_type16 = df_data['type_capacity16'].str.cat(df_data[['area16',
-                                                        'room16',
-                                                        'toilet16',
-                                                        'structure16',
-                                                        'n_this_area16']], sep=',')
+df['전용면적(㎡)'] = area_inform2.str.get(0)
+df['전용면적(㎡)'] = df['전용면적(㎡)'].str.slice(start=0, stop=-1)
+df['전용면적(㎡)'] = pd.to_numeric(df['전용면적(㎡)'])
 
-se_type17 = df_data['type_capacity17'].str.cat(df_data[['area17',
-                                                        'room17',
-                                                        'toilet17',
-                                                        'structure17',
-                                                        'n_this_area17']], sep=',')
+df['전용률(%)'] = area_inform2.str.get(1)
+df['전용률(%)'] = df['전용률(%)'].str.slice(start=4, stop=-2)
 
-# 오류발생 가능
-# type_capacity_number 의 모든 값이 nan 으로 채워져있을 경우 에러가 발생함
+# 방, 화장실 개수
+df['방 개수'] = df_seoul['room'].str.slice(start=0, stop=-1)
+df['방 개수'] = pd.to_numeric(df['방 개수'])  # 오류발생
 
-# 해당 오류 발생시 밑의 코드를 탄력적으로 수정
-# 원본
-#df_Gu = pd.concat([df_Gu, se_type1, se_type2, se_type3, se_type4, se_type5, se_type6, se_type7, se_type8,
-#                  se_type9, se_type10, se_type11, se_type12, se_type13, se_type14, se_type15, se_type16, se_type17],
-#                  axis=1)
+df['화장실 개수'] = df_seoul['toilet'].str.slice(start=0, stop=-1)
+df['화장실 개수'] = pd.to_numeric(df['화장실 개수'])  # 오류발생
 
+# 구조, 해당면적 세대수
+df['구조'] = df_seoul['structure']
 
-df_Gu = pd.concat([df_Gu, se_type1, se_type2, se_type3, se_type4, se_type5, se_type6, se_type7, se_type8,
-                   se_type9, se_type10, se_type11, se_type12, se_type13],
-                  axis=1)  # 만들어 둔 시리즈를 기존의 데이터 프레임에 합치기
+df['해당면적 세대수'] = df_seoul['n_this_area'].str.slice(start=0, stop=-2)
+df['해당면적 세대수'] = pd.to_numeric(df['해당면적 세대수'])  # 오류발생
 
-#############################################################################################################
+# 해당면적 세대수, 화장실 개수, 방 개수, 공급면적이 숫자로 처리되지 않고 문자열로 처리되어 있는 상태
+# 확인 및 변경이 요구됨
 
-# 하나의 컬럼으로 만들어 둔 면적별 정보(type_capacity)에 따라 아파트 기본 정보들을 stack 시키기
-# 에러 처리
-# 원본
-#df_edit = df_Gu.melt(id_vars=['읍면동', '아파트', '세대수', '입주년월', 'Apt_name', 'number', 'floor',
-#                              'confirm_date', 'car', 'FAR', 'BC', 'con', 'heat', 'code', 'lat',
-#                              'long'], value_vars=['type_capacity1', 'type_capacity2', 'type_capacity3',
-#                                                   'type_capacity4', 'type_capacity5', 'type_capacity6',
-#                                                   'type_capacity7', 'type_capacity8', 'type_capacity9',
-#                                                   'type_capacity10', 'type_capacity11', 'type_capacity12',
-#                                                   'type_capacity13', 'type_capacity14', 'type_capacity15',
-#                                                   'type_capacity16', 'type_capacity17'])
-
-# 수정
-df_edit = df_Gu.melt(id_vars=['읍면동', '아파트', '세대수', '입주년월', 'Apt_name', 'number', 'floor',
-                              'confirm_date', 'car', 'FAR', 'BC', 'con', 'heat', 'code', 'lat',
-                              'long'], value_vars=['type_capacity1', 'type_capacity2', 'type_capacity3',
-                                                   'type_capacity4', 'type_capacity5', 'type_capacity6',
-                                                   'type_capacity7', 'type_capacity8', 'type_capacity9',
-                                                   'type_capacity10', 'type_capacity11', 'type_capacity12',
-                                                   'type_capacity13'])
-
-############################################################################################
-
-df_edit = df_edit.sort_values(by='Apt_name')  # 아파트 이름에 따라 행을 정렬
-df_edit2 = df_edit.dropna(axis=0)  # nan 값이 있는 행 제거: 면적이 3개인 아파트의 경우 4~10번째 면적정보는 drop 된다.
-
-type_information = df_edit2['value'].str.split(',')  # 하나의 컬럼으로 만들어둔 면적별 정보를 다시 6개로 나누기
-
-# 데이터 프레임에 각각 고유의 이름으로 합치기
-df_edit2['type_capacity'] = type_information.str.get(0)
-df_edit2['area'] = type_information.str.get(1)
-df_edit2['room'] = type_information.str.get(2)
-df_edit2['toilet'] = type_information.str.get(3)
-df_edit2['structure'] = type_information.str.get(4)
-df_edit2['n_this_area'] = type_information.str.get(5)
-
-df_Gu_last = df_edit2.drop(['value', 'variable', '아파트',
-                            '세대수', '입주년월'], axis=1)  # 필요없어진 값 제거
-
-df_Gu_last.insert(0, 'Gu', '도봉구')  # 이름 확인 주의
-
-df = df_Gu_last[['Gu', '읍면동', 'Apt_name']]
-
-df[['세대수', '동 개수']] = df_Gu_last['number'].str.split('(', n=1, expand=True)  # number 분리
-df[['저층', '고층']] = df_Gu_last['floor'].str.split('/', expand=True)
-#df['사용승인일'] = pd.to_datetime(df_Gu_last['confirm_date'])
-df[['주차대수_총', '주차대수_세대당']] = df_Gu_last['car'].str.split('/', n=1, expand=True)
-df['용적률'] = df_Gu_last['']
-
-
-# df = df_Gu_last.set_index('Apt_name')  # 비교가 용이하도록 Apt_name 을 인덱스로 설정
-
-df.to_excel('Gangbuk_edit4/Dobonggu_edit4.xlsx', sheet_name='edit1', index=True)  # 이름 확인 주의
-
-
-df = df_Gu_last[['Gu', '읍면동', 'Apt_name']]
-
-df[['세대수', '동 개수']] = df_Gu_last['number'].str.split('(', n=1, expand=True)  # number 분리
-df[['저층', '고층']] = df_Gu_last['floor'].str.split('/', expand=True)
-#df['사용승인일'] = pd.to_datetime(df_Gu_last['confirm_date'])
-df[['주차대수_총', '주차대수_세대당']] = df_Gu_last['car'].str.split('/', n=1, expand=True)
-df['용적률'] = df_Gu_last['']
+df.to_excel('Seoul.xlsx', sheet_name='total', index=False)
