3739f0bcc1514aeb23eb47206535eeceb9ad1e3a
Kimyihwan <kimleehwan3@gmail.com>
2021-04-26 00:32:03 +0900

    20210425 추가

--- melt_20210423_third.py
+++ melt_20210423_third.py
@@ -63,19 +63,28 @@ df_GN = pd.concat(dfs2, axis=0)  # axis=0 : 밑으로 붙이기
 
 # 강북데이터와 강남데이터를 각각 edit4 폴더에 새롭게 저장한 후 합치기
 
-df_GB.to_excel('Gangbuk_edit4/Gangbuk_total.xlsx', sheet_name='Gangbuk', index=False)
-df_GN.to_excel('Gangnam_edit4/Gangnam_total.xlsx', sheet_name='Gangnam', index=False)
+# df_GB.to_excel('Gangbuk_edit4/Gangbuk_total.xlsx', sheet_name='Gangbuk', index=False)
+# df_GN.to_excel('Gangnam_edit4/Gangnam_total.xlsx', sheet_name='Gangnam', index=False)
+
+df_GB = pd.read_excel('Gangnam_edit4/Gangnam_total.xlsx', header=0, skipfooter=0)
+df_GN = pd.read_excel('Gangnam_edit4/Gangnam_total.xlsx', header=0, skipfooter=0)
+
 
 df_seoul = pd.concat([df_GB, df_GN], axis=0)
+
+df_seoul = df_seoul.sort_values(by=['Apt_name'])
+df_seoul = df_seoul.reset_index(drop='Ture')
 
 # seoul data 편집하기
 
 df_seoul.replace('-', np.nan)
 
 df = df_seoul[['Gu', '읍면동', 'Apt_name']]
+df.columns = ['지역구', '법정동', '아파트']
 
 # number 편집
-df['세대수'] = df_seoul['number'].str.split('(', n=1, expand=True)
+se_number = df_seoul['number'].str.split('(', n=1, expand=True)
+df['세대수'] = se_number[0]
 df['세대수'] = df['세대수'].str.slice(start=0, stop=-2)
 df['세대수'] = pd.to_numeric(df['세대수'])
 
@@ -115,7 +124,7 @@ df['면적유형'] = df_seoul['type_capacity']
 area_inform = df_seoul['area'].str.split('/')
 df['공급면적(㎡)'] = area_inform.str.get(0)
 df['공급면적(㎡)'] = df['공급면적(㎡)'].str.slice(start=0, stop=-1)
-df['공급면적(㎡)'] = pd.to_numeric(df['공급면적(㎡)'])  # 오류발생
+# df['공급면적(㎡)'] = pd.to_numeric(df['공급면적(㎡)'])  # 오류발생
 
 area_inform2 = area_inform.str.get(1)
 area_inform2 = area_inform2.str.split('(')
@@ -129,18 +138,37 @@ df['전용률(%)'] = df['전용률(%)'].str.slice(start=4, stop=-2)
 
 # 방, 화장실 개수
 df['방 개수'] = df_seoul['room'].str.slice(start=0, stop=-1)
-df['방 개수'] = pd.to_numeric(df['방 개수'])  # 오류발생
+# df['방 개수'] = pd.to_numeric(df['방 개수'])  # 오류발생
 
 df['화장실 개수'] = df_seoul['toilet'].str.slice(start=0, stop=-1)
-df['화장실 개수'] = pd.to_numeric(df['화장실 개수'])  # 오류발생
+# df['화장실 개수'] = pd.to_numeric(df['화장실 개수'])  # 오류발생
 
 # 구조, 해당면적 세대수
 df['구조'] = df_seoul['structure']
 
 df['해당면적 세대수'] = df_seoul['n_this_area'].str.slice(start=0, stop=-2)
-df['해당면적 세대수'] = pd.to_numeric(df['해당면적 세대수'])  # 오류발생
+# df['해당면적 세대수'] = pd.to_numeric(df['해당면적 세대수'])  # 오류발생
+
+# 사용승인일 참고[https://m.blog.naver.com/wideeyed/221603778414]
+df['사용승인일'] = df_seoul['confirm_date']
+df.replace({'사용승인일': {'년 ': '-'}}, inplace=True)
+df['사용승인일'] = df['사용승인일'].str.replace(pat='년 ', repl='-', regex=False)
+df['사용승인일'] = df['사용승인일'].str.replace(pat='월', repl='', regex=False)
+df['사용승인일'] = df['사용승인일'].str.replace(pat='월 ', repl='-', regex=False)
+df['사용승인일'] = df['사용승인일'].str.replace(pat='일', repl='', regex=False)
+
+df['사용승인일'] = pd.to_datetime(df['사용승인일'])
 
 # 해당면적 세대수, 화장실 개수, 방 개수, 공급면적이 숫자로 처리되지 않고 문자열로 처리되어 있는 상태
 # 확인 및 변경이 요구됨
 
+
+# 열 순서 변경
+df = df[['지역구', '법정동', '아파트', '아파트코드', '사용승인일', '세대수', '저층', '고층', '주차대수_총', '주차대수_세대', '용적률',
+         '건폐율', '위도', '경도', '건설사', '난방', '구조', '면적유형', '해당면적 세대수',
+         '공급면적(㎡)', '전용면적(㎡)', '전용률(%)', '방 개수',
+         '화장실 개수']]
+
 df.to_excel('Seoul.xlsx', sheet_name='total', index=False)
+
+
