39695951e77552b44d45f719ca7ec956171d4341
Kimyihwan <kimleehwan3@gmail.com>
2021-04-26 21:34:08 +0900

    20210426 추가

---
+++ main_code/main_20210426_prepare_seminar.py
@@ -0,0 +1,15 @@
+# import packages
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel('Seoul_last.xlsx', header=0, skipfooter=0)
+
+df1 = df
+df2 = df
+
+df1 = df1.drop_duplicates(['아파트코드'], keep='first')
+df1 = df1.sort_values(by=['지역구'])
+
+df1.to_excel('Seoul_last_sorting.xlsx', sheet_name='last', index=False)
+
+df2.to_excel('Seoul_last.xlsx', sheet_name='last', index=False)
