ede25aa71883120eb948fd23ea2463eb158b64ed
Kimyihwan <kimleehwan3@gmail.com>
2021-04-28 02:43:42 +0900

    20210428 추가

--- main_code/main_20210426_refine.py
+++ main_code/main_20210426_refine.py
@@ -72,4 +72,8 @@ df_seoul['화장실 개수'] = pd.to_numeric(df_seoul['화장실 개수'])
 
 df_seoul.dtypes
 
-df_seoul.to_excel('Seoul_last.xlsx', sheet_name='last', index=False)
+df_seoul = df_seoul[['지역구', '법정동', '아파트', '아파트코드', '사용승인일', '연수', '세대수', '저층', '고층', '주차대수_총',
+                     '주차대수_세대', '용적률', '건폐율', '위도', '경도', '건설사', '난방', '구조', '면적유형',  '전용면적(㎡)',
+                     '전용률(%)', '방 개수', '화장실 개수']]
+
+df_seoul.to_excel('seminar data/Seoul_last.xlsx', sheet_name='last', index=False)
