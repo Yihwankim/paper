749d431b00d23d3eef5ed863b84a1f10d233e6da
Kimyihwan <kimleehwan3@gmail.com>
2021-04-28 02:50:27 +0900

    20210428 추가

--- main_code/main_20210426_prepare_seminar.py
+++ main_code/main_20210426_prepare_seminar.py
@@ -10,8 +10,11 @@ df_description2 = df
 df_description2 = df_description2.drop_duplicates(['아파트코드'], keep='first')
 df_description2 = df_description2.sort_values(by=['지역구'])
 
-df_description1 = df_description1[['주차대수_세대', '전용면적(㎡)', '전용률(%)', '방 개수', '화장실 개수']]
-df_description2 = df_description2[['연수', '세대수', '저층', '고층', '주차대수_총', '용적률', '건폐율']]
+df_description1 = df_description1[['지역구', '법정동', '아파트', '주차대수_세대',
+                                   '전용면적(㎡)', '전용률(%)', '방 개수', '화장실 개수']]
+
+df_description2 = df_description2[['지역구', '법정동', '아파트', '연수', '세대수', '저층',
+                                   '고층', '주차대수_총', '용적률', '건폐율']]
 
 df_description1.to_excel('seminar data/Seoul_last_description_type.xlsx', sheet_name='total', index=False)
 df_description2.to_excel('seminar data/Seoul_last_description_apt.xlsx', sheet_name='total', index=False)
